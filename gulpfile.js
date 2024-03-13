d6a9b04a0cf8cee928b55123b290eb90281b0445
Alexey Mayants <a.mayants@cityads.ru>
2024-03-13 14:29:06 +0300

    Migrate to gulp-sass@5 + sass, fix division deprecation warnings

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
 var cleanCss = require('gulp-clean-css');
